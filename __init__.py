696b9b37fcbd7c19725b74fca5e66e067a1b050c
Matthew Piccolella <mjp2220@columbia.edu>
2014-03-20 22:16:42 -0400

    Added keys, basic requests and others

--- __init__.py
+++ __init__.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+
 app = Flask(__name__)
+
+GOOGLE_API_KEY = "AIzaSyDhoZ2Yyii_wvZaWSqUu4BilsVAfJHZIzk"
+GOOGLE_PLACES_BASE_STRING = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+GOOGLE_DETAILS_BASE_STRING = "https://maps.googleapis.com/maps/api/place/details/json?reference="
+GOOGLE_MAPS_BASE_STRING = "http://maps.googleapis.com/maps/api/staticmap?"
+
+COLUMBIA_LAT = "40.807001"
+COLUMBIA_LONG = "-73.9640299"
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    url = GOOGLE_PLACES_BASE_STRING + COLUMBIA_LAT + "," + COLUMBIA_LONG + "&radius=10000&types=museum&sensor=false&key=" + GOOGLE_API_KEY
+    response_dict = requests.get(url).json()
+    return jsonify(response_dict)
 
 if __name__ == "__main__":
     app.run()
